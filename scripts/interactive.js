6a1da71b9f9d12597aeb79c4bb81d80b41511f6d
viktoriiachorna04 <viktoriia.chorna04@gmail.com>
2026-05-24 21:40:02 +0300

    Update interactive.js

--- scripts/interactive.js
+++ scripts/interactive.js
@@ -1,1 +1,25 @@
+function showMessage(){
 
+    document.getElementById("result").innerHTML =
+    "Так, це фішингове повідомлення, оскільки воно містить підозріле посилання та заклик терміново перейти за ним.";
+
+}
+
+function checkAnswer(answer){
+
+    const result = document.getElementById("test-result");
+
+    if(answer === "correct"){
+
+        result.innerHTML = "✅ Правильно! Це надійний пароль.";
+
+        result.style.color = "green";
+
+    }else{
+
+        result.innerHTML = "❌ Неправильно. Такий пароль легко зламати.";
+
+        result.style.color = "red";
+
+    }
+}
